c55ac212088e301a8e30e95933722b38b4042a6d
Edward Bradley <edbrad@edbrad.com>
2018-05-12 10:04:34 -0500

    Initialize .NET Core Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bl_status_svc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
